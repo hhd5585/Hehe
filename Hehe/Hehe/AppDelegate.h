5d8f233aa0d4696c22be81384cd02835f4ffe498
zxm <zxm@zxmdemacbook-pro.local>
2015-11-20 14:57:14 +0800

    新建项目

---
+++ Hehe/Hehe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hehe
+//
+//  Created by zxm on 15/11/20.
+//  Copyright © 2015年 wquant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hehe/Hehe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hehe
+//
+//  Created by zxm on 15/11/20.
+//  Copyright © 2015年 wquant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
